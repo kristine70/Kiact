19a54b2543f330eb142d1d94cf5b9b9b555fa4a7
Kristine70 <kristine.liang@outlook.com>
2025-03-14 11:54:09 -0700

    feat(kiact): implement createElement function and KiactElement type definition

---
+++ packages/kiact/src/JSXElement.ts
@@ -0,0 +1,31 @@
+import {KiactElementType} from '@types';
+type k = string | number | null;
+const REACT_ELEMENT_TYPE: symbol = Symbol.for('react.transitional.element');
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function KiactElement(type: any, key: k, props: any): KiactElementType {
+  const element: KiactElementType = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref: props.ref || null,
+    props,
+  };
+
+  return element;
+}
+
+export function createElement(type, config, ...children) {
+  const props = {...config, children};
+  const key = config.key === null ? null : String(config.key);
+
+  // apply default props to the KiactElement
+  if (type && type.defaultProps) {
+    const defaultProps = type.defaultProps;
+    for (propName in defaultProps) {
+      if (props[propName] === undefined)
+        props[propName] = defaultProps[propName];
+    }
+  }
+  return KiactElement(type, key, props);
+}
--- packages/types/KiactElement.type.ts
+++ packages/types/KiactElement.type.ts
@@ -1,9 +1,11 @@
-export interface KiactElement {
-  type: string;
-  props: {
-    children: KiactElement[];
-  };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface KiactElementType<P = any, T = any> {
+  /** type is flexible — it can be a string, function, class, or special React object.*/
+  type: T;
+  props: P;
   key: string | number | null;
-  ref: HTMLElement | null;
+
+  // A ref object or callback for referencing DOM elements or component instances.
+  ref: ((instance: unknown) => void) | null | {current: unknown} | null;
   $$typeof: symbol;
 }
